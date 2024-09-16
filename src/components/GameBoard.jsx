8085e26011f88fc1265bf99a023f12a5f9cb81bc
kxtmg <disizkatie@gmail.com>
2024-09-16 11:22:38 -0700

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -20,7 +20,11 @@ export default function GameBoard({ onSelectSquare, board }) {
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}> {playerSymbol} </button>
+                        <button 
+                        onClick={() => onSelectSquare(rowIndex, colIndex)}
+                        disabled={playerSymbol !== null}> 
+                            {playerSymbol} 
+                        </button>
                     </li>)}
                 </ol>
             </li>)}
