486f239ec2a976c1c68d997d064e016d590292a7
kxtmg <77362684+kxtmg@users.noreply.github.com>
2024-09-16 16:36:42 -0700

    Final Polish & Improving Components

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
-
-export default function GameBoard({ onSelectSquare, turns }) {
+export default function GameBoard({ onSelectSquare, board }) {
 
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
     // function handleSelectSquare(rowIndex, colIndex) {
@@ -19,25 +13,14 @@ export default function GameBoard({ onSelectSquare, turns }) {
     //     onSelectSquare();
     // }
 
-    // Deriving State
-    let gameBoard = initialGameBoad;
-
-    for(const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
-
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => 
+            {board.map((row, rowIndex) => 
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}> {playerSymbol} </button>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}> {playerSymbol} </button>
                     </li>)}
                 </ol>
             </li>)}
